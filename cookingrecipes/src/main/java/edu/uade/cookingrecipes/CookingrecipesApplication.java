360c983d16b2e719c93d0f363fefcf7ef7e389cb
Felipe Parrondo Bastos <feliparrondobastos@gmail.com>
2025-03-18 14:07:00 -0300

    spring initialzr

---
+++ cookingrecipes/src/main/java/edu/uade/cookingrecipes/CookingrecipesApplication.java
@@ -0,0 +1,13 @@
+package edu.uade.cookingrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingrecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingrecipesApplication.class, args);
+	}
+
+}
---
+++ cookingrecipes/src/test/java/edu/uade/cookingrecipes/CookingrecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uade.cookingrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingrecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
