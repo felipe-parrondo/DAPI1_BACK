ba657c04e5617adc3b50e6f64a05afd50bdca666
Felipe Parrondo Bastos <feliparrondobastos@gmail.com>
2025-05-21 23:25:23 -0300

    added the AuthenticationController logic

--- cookingrecipes/src/main/java/edu/uade/cookingrecipes/service/implementation/AuthenticationServiceImpl.java
+++ cookingrecipes/src/main/java/edu/uade/cookingrecipes/service/implementation/AuthenticationServiceImpl.java
@@ -2,7 +2,13 @@ package edu.uade.cookingrecipes.service.implementation;
 
 import edu.uade.cookingrecipes.dto.auth.AuthenticationRequestDto;
 import edu.uade.cookingrecipes.dto.auth.AuthenticationResponseDto;
+import edu.uade.cookingrecipes.dto.auth.ChangePasswordRequestDto;
+import edu.uade.cookingrecipes.dto.auth.CreateCodeRequestDto;
+import edu.uade.cookingrecipes.dto.auth.CreateCodeResponseDto;
 import edu.uade.cookingrecipes.dto.auth.RegisterRequestDto;
+import edu.uade.cookingrecipes.dto.auth.ValidateCodeRequestDto;
+import edu.uade.cookingrecipes.dto.auth.ValidateCodeResponseDto;
+import edu.uade.cookingrecipes.dto.auth.ValidateRegisterRequestDto;
 import edu.uade.cookingrecipes.service.AuthenticationService;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -15,5 +21,25 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public AuthenticationResponseDto register(RegisterRequestDto registerRequest) {
         return null; //TODO
+    }
+
+    @Override
+    public void validateRegister(ValidateRegisterRequestDto validateCodeRequest) {
+        //TODO
+    }
+
+    @Override
+    public CreateCodeResponseDto createCode(CreateCodeRequestDto createCodeRequest) {
+        return null; //TODO
+    }
+
+    @Override
+    public ValidateCodeResponseDto validateCode(ValidateCodeRequestDto validateCodeRequest) {
+        return null; //TODO
+    }
+
+    @Override
+    public void changePassword(ChangePasswordRequestDto changePasswordRequest) {
+        //TODO
     }
 }
