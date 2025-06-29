1f22e61ecac22aae5b1d28c3b069338debbac539
Olmos2121 <tomyolmos1@hotmail.com>
2025-06-29 19:34:06 -0300

    Metodo getUser
    Get user con todos los datos incluido metodo de pago, sin contraseña

--- cookingrecipes/src/main/java/edu/uade/cookingrecipes/controller/UserController.java
+++ cookingrecipes/src/main/java/edu/uade/cookingrecipes/controller/UserController.java
@@ -1,10 +1,19 @@
 package edu.uade.cookingrecipes.controller;
 
+import edu.uade.cookingrecipes.dto.response.UserResponseDto;
+import edu.uade.cookingrecipes.model.UserModel;
+import edu.uade.cookingrecipes.service.UserService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Api(value = "User Operations")
 @Validated
@@ -12,5 +21,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-    //TODO recordar que el POST para crear tambien funciona como PUT
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/") // Get all users
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
+        List<UserResponseDto> users = userService.getAllUsers();
+        if (users != null && !users.isEmpty()) {
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/{userId}") // Get user by ID
+    public ResponseEntity<UserResponseDto> getUserById(Long userId) {
+        UserResponseDto user = userService.getUserById(userId);
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/me") // Get user by token
+    public ResponseEntity<UserResponseDto> getUserByToken() {
+        UserResponseDto user = userService.getUserByToken();
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
 }
